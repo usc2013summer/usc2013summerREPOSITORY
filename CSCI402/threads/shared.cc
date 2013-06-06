1e2f3d5ff077de6647fea2dc04d9c87c1a2f567e
Dongyoung Jung <dongyouj@usc.edu>
2013-06-06 15:45:38 -0700

    shared & makefile

--- CSCI402/threads/shared.cc
+++ CSCI402/threads/shared.cc
@@ -1,9 +1,15 @@
-#define VISITOR_MAX 4
-#define CAR_MAX 3
-#define LIMO_MAX 4
-#define VALET_MAX 5
-
-Lock* carLock[CAR_MAX];
-Lock* limoLock[LIMO_MAX];
-Condition* carCV[CAR_MAX];
-Condition* limoCV[LIMO_MAX];
+int totalVehicle;
+Lock* lineLock;
+List* limoQ, carQ;
+List* valetQ;	
+Lock* keyLock[];
+Condition* keyCV[];
+Lock* vehicleLock[];
+Condition* vehicleCV[];
+Lock* tokenLock[];
+Condition* tokenCV[];
+Lock* readingLock[];
+Condition* readingCV[];
+Lock* tipLock[];
+Condition* tipCV[];
+List* leavingQ;
